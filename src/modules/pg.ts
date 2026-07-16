9861f82dfb040b43ee221f2d81719091c7bf221b
ALiangNe <2650321653@qq.com>
2026-07-16 15:30:25 +0800

    feature: decouple business logic from database pool by adding getPgClient helper

--- src/modules/pg.ts
+++ src/modules/pg.ts
@@ -8,58 +8,38 @@ import { POSTGRES_ERRORS } from '../errors/postgres'
 export let pgClients: Partial<Record<DataRegion, Pool>> = {}
 
 /**
- * Initialize PostgreSQL clients for data regions
- * @param configs PostgreSQL configurations by region
+ * Initialize PostgreSQL clients
+ * @param configs PostgreSQL configurations
  */
 export const initPgClients = async (configs: Record<DataRegion, PgCredential>) => {
-    const usw1Config = configs.usw1
-    const euc1Config = configs.euc1
-    if (!usw1Config.PG_HOST || !usw1Config.PG_PORT || !usw1Config.PG_USERNAME || !usw1Config.PG_PASSWORD || !usw1Config.PG_DATABASE) throw 'INVALID_POSTGRES_CREDENTIALS'
-    if (!euc1Config.PG_HOST || !euc1Config.PG_PORT || !euc1Config.PG_USERNAME || !euc1Config.PG_PASSWORD || !euc1Config.PG_DATABASE) throw 'INVALID_POSTGRES_CREDENTIALS'
+    const clients: Partial<Record<DataRegion, Pool>> = {}
+    for (const [region, config] of Object.entries(configs) as [DataRegion, PgCredential][]) {
+        const { PG_HOST, PG_PORT, PG_USERNAME, PG_PASSWORD, PG_DATABASE, PG_MAX_CONNECTIONS, PG_USE_TLS } = config
+        if (!PG_HOST || !PG_PORT || !PG_USERNAME || !PG_PASSWORD || !PG_DATABASE) throw 'INVALID_POSTGRES_CREDENTIALS'
 
-    const usw1Client = new Pool({
-        host: usw1Config.PG_HOST,
-        port: usw1Config.PG_PORT,
-        user: usw1Config.PG_USERNAME,
-        password: usw1Config.PG_PASSWORD,
-        database: usw1Config.PG_DATABASE,
-        max: usw1Config.PG_MAX_CONNECTIONS,
-        connectionTimeoutMillis: 5000,
-        ssl: usw1Config.PG_USE_TLS ? { rejectUnauthorized: false } : false,
-    })
-    const euc1Client = new Pool({
-        host: euc1Config.PG_HOST,
-        port: euc1Config.PG_PORT,
-        user: euc1Config.PG_USERNAME,
-        password: euc1Config.PG_PASSWORD,
-        database: euc1Config.PG_DATABASE,
-        max: euc1Config.PG_MAX_CONNECTIONS,
-        connectionTimeoutMillis: 5000,
-        ssl: euc1Config.PG_USE_TLS ? { rejectUnauthorized: false } : false,
-    })
+        const client = new Pool({
+            host: PG_HOST,
+            port: PG_PORT,
+            user: PG_USERNAME,
+            password: PG_PASSWORD,
+            database: PG_DATABASE,
+            max: PG_MAX_CONNECTIONS,
+            connectionTimeoutMillis: 5000,
+            ssl: PG_USE_TLS ? { rejectUnauthorized: false } : false,
+        })
 
-    let usw1Connection: PoolClient | null = null
-    try {
-        usw1Connection = await usw1Client.connect()
-    } catch (e) {
-        console.error('Failed connect PostgreSQL usw1', e)
-        throw 'FAILED_CONNECT_PG_CLIENT'
+        let connection: PoolClient | null = null
+        try {
+            connection = await client.connect()
+        } catch (e) {
+            console.error(`Failed connect PostgreSQL ${region}`, e)
+            throw 'FAILED_CONNECT_PG_CLIENT'
+        }
+        if (connection) connection.release()
+
+        clients[region] = client
     }
-    if (usw1Connection) usw1Connection.release()
-
-    let euc1Connection: PoolClient | null = null
-    try {
-        euc1Connection = await euc1Client.connect()
-    } catch (e) {
-        console.error('Failed connect PostgreSQL euc1', e)
-        throw 'FAILED_CONNECT_PG_CLIENT'
-    }
-    if (euc1Connection) euc1Connection.release()
-
-    pgClients = {
-        usw1: usw1Client,
-        euc1: euc1Client,
-    }
+    pgClients = clients
 }
 
 /**
